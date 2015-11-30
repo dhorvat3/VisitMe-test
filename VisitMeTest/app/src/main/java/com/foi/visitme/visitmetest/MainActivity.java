53cd01a9f8ae127295c835ddf2b706a96d51c8ba
fijelicic <fijelicic@gmail.com>
2015-11-30 16:51:18 +0100

    update

--- VisitMeTest/app/src/main/java/com/foi/visitme/visitmetest/MainActivity.java
+++ VisitMeTest/app/src/main/java/com/foi/visitme/visitmetest/MainActivity.java
@@ -2,12 +2,42 @@ package com.foi.visitme.visitmetest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.foi.visitme.visitmetest.db.Apartments;
+import com.foi.visitme.visitmetest.db.ApiMethods;
+
+import retrofit.Callback;
+import retrofit.RestAdapter;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String API_URL = "http://domotehnika.hr/visitme/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RestAdapter restAdapter = new RestAdapter.Builder()
+                .setEndpoint(API_URL)
+                .build();
+        ApiMethods apiMethods = restAdapter.create(ApiMethods.class);
+
+        apiMethods.getApartments("1", new Callback<Apartments>() {
+            @Override
+            public void success(Apartments apartments, Response response) {
+                TextView ispis=(TextView) findViewById(R.id.ispis);
+                ispis.setText(apartments.getApartment().get(0).getApartmentName());
+
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+
+            }
+        });
     }
+
+
 }
---
+++ VisitMeTest/db/src/main/java/com/foi/visitme/visitmetest/db/ApiMethods.java
@@ -0,0 +1,22 @@
+package com.foi.visitme.visitmetest.db;
+
+import retrofit.Callback;
+import retrofit.client.Response;
+import retrofit.http.Body;
+import retrofit.http.GET;
+import retrofit.http.POST;
+import retrofit.http.Query;
+
+/**
+ * Created by Filip on 30.11.2015..
+ */
+public interface ApiMethods {
+
+    //LOGIN
+    @GET("/apartments_in_town.php")
+    public void getApartments(@Query("id_town") String id_town, Callback<Apartments> callback);
+
+//    REGISTER
+//    @POST("/post_user.php")
+//    void setUser(@Body User_ user, Callback<Response> callback);
+}
